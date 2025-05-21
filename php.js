9545b92d4f9c1873b06b027340e733e3da58e61a
Minewoolf42 <kir.belyakov.11@gmail.com>
2025-05-21 16:50:54 +0200

    Update php.js

--- php.js
+++ php.js
@@ -1,6 +1,6 @@
 function fetchjson(mode,data){
     xhr = new XMLHttpRequest()
-    if(mode=='GET'){url='http://f1059512.xsph.ru/Nikolaychik/loader.php'}else{url='http://f1059512.xsph.ru/Nikolaychik/saver.php'}
+    if(mode=='GET'){url='https://minewoolf42.github.io/Nikolaychik/loader.php'}else{url='https://minewoolf42.github.io/Nikolaychik/saver.php'}
     xhr.open(mode, url, true)
     xhr.send(JSON.stringify(data))
     xhr.responseType = 'json'
